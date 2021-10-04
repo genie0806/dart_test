197b919532206038488260444c66ced50b9171d3
genie0806 <genie0806@gmail.com>
2021-10-05 02:49:58 +0900

    cleric 클래스 예제

--- bin/dart_test.dart
+++ bin/dart_test.dart
@@ -1,4 +1,26 @@
-void main(List<String> arguments) {
-  print('세계 안녕');
-  print('세계 안녕');
+class Hero {
+  String name;
+  int hp;
+
+  Hero(this.name, this.hp);
+  void attack() {
+    print('공격했다.');
+  }
+}
+
+class Cleric extends Hero {
+  static final int maxHp = 50;
+  static final int maxMp = 10;
+  int mp = maxMp;
+
+  Cleric(String name, int hp) : super(name, hp);
+}
+
+void main() {
+  final hong = Hero('홍길동', 50);
+  final lim = Hero('임꺽정', 50);
+  print(hong.hashCode);
+  print(lim.hashCode);
+  final cleric = Cleric('안데르센 신부', 1000);
+  print(cleric.name);
 }
